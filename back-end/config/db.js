24581fbdfeabbc9dc42dccd76cc7412d1b66861d
khutso-student <khutsomakunyane1@gmail.com>
2025-08-01 11:53:55 +0200

    db file updated

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected Successfully');
-    } catch (error) {
-        console.error('MongoDB connection failed:', error.message)
-        process.exit(1);
-    }
+  try {
+    const uri = process.env.NODE_ENV === 'production'
+      ? process.env.MONGO_URI_PROD
+      : process.env.MONGO_URI_LOCAL;
+
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB connected successfully to ${process.env.NODE_ENV} database`);
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
